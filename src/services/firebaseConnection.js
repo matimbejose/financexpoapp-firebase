cf1155f92ec91a36eecf42812bd2eadf61d3257a
José Justino Matimbe <81678247+matimbejose@users.noreply.github.com>
2023-07-16 11:51:39 +0200

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -5,17 +5,6 @@ import 'firebase/compat/database'
 
 
 let firebaseConfig = {
-    apiKey: "AIzaSyDr5P-d0HPFPLa5GOiDQZw8hS-M_eOD8LY",
-
-    authDomain: "financasv2.firebaseapp.com",
-
-    projectId: "financasv2",
-
-    storageBucket: "financasv2.appspot.com",
-
-    messagingSenderId: "588434114852",
-
-    appId: "1:588434114852:web:8268f997337d376b82cbb1"
 
 };
 
